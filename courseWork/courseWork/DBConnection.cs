5115a4ef3edb5b1e07d12aa2fcebdcbca91c916d
vyshyvan_v <vyshyvanv.official@gmail.com>
2019-03-09 21:10:05 +0200

    adding db w/ singleTon

---
+++ courseWork/courseWork/DBConnection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace courseWork
+{
+    class DBConnection
+    {
+        private static DBConnection instance;
+
+        private DBConnection()
+        { }
+
+        public static DBConnection getInstance()
+        {
+            if (instance == null)
+                instance = new DBConnection();
+
+            //SqlConnection con = new SqlConnection();
+            //con.ConnectionString = ConfigurationManager.ConnectionStrings["pricesTable"].ConnectionString;
+            //con.Open();
+            //SqlCommand cmd = new SqlCommand();
+            //cmd.CommandText = "select * from dbo.prices";
+            //cmd.Connection = con;
+            //SqlDataAdapter da = new SqlDataAdapter(cmd);
+            //DataTable dt = new DataTable("dbo.prices");
+            //da.Fill(dt);
+
+            //g1.ItemsSource = dt.DefaultView;
+            //instance = da;
+
+            return instance;
+        }
+    }
+}
---
+++ courseWork/courseWork/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace courseWork
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected void LoadDBAndChangeWindow()
+        {
+            DBConnection dBConnection = DBConnection.getInstance();
+            Window1 w = new Window1();
+            w.Show();
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoadDBAndChangeWindow();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            LoadDBAndChangeWindow();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
